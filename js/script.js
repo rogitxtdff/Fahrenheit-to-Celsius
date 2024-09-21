a183b753752021438b510292c02ca1384aea288a
mohamad <rogitxtdff@gmail.com>
2024-09-21 14:10:05 +0330

    chore: Add Responsive

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ let H1Elem = $.querySelector("#h1");
 
 let IsTrue = true;
 
+InputUser.value = ''
+
 ChangeElem.addEventListener("click", function () {
   if (IsTrue) {
     H1Elem.innerHTML = "Convert °F to °C";
@@ -27,30 +29,30 @@ ChangeElem.addEventListener("click", function () {
 
 ResetElem.addEventListener("click", function () {
   InputUser.value = "";
-  AlertUser.style.display = "none";
+  AlertUser.style.opacity = '0%'
 });
 
 ConvertElem.addEventListener("click", function () {
   var INPUT = InputUser.value;
   if (isNaN(INPUT)) {
     AlertUser.innerHTML = "Write correct value!";
-    AlertUser.style.display = "block";
-    AlertUser.style.color = "red";
+    AlertUser.style.opacity = '100%'
+    AlertUser.style.color = "#d55252";
   } else if (INPUT === "") {
     AlertUser.innerHTML = "Write correct value!";
-    AlertUser.style.display = "block";
-    AlertUser.style.color = "red";
+    AlertUser.style.opacity = '100%'
+    AlertUser.style.color = "#d55252";
   } else {
     if (IsTrue) {
       let Result = INPUT * 1.8 + 32;
       AlertUser.innerHTML = INPUT + "°C  to  " + Result.toFixed(1) + "°F";
-      AlertUser.style.display = "block";
-      AlertUser.style.color = "green";
+      AlertUser.style.opacity = '100%'
+      AlertUser.style.color = "#60ca60";
     } else {
       let Result = (INPUT - 32) / 1.8;
       AlertUser.innerHTML = INPUT + "°F  to  " +Result.toFixed(1) + "°C";
-      AlertUser.style.display = "block";
-      AlertUser.style.color = "green";
+      AlertUser.style.opacity = '100%'
+      AlertUser.style.color = "#60ca60";
     }
   }
 });
